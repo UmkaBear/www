d4e4abde44dcc09e37a727a3e90c24774d1a8835
UmkaBear <dfakhiev@yandex.ru>
2024-03-30 16:46:14 +0500

    Создал структуру папок проекта и задал шаблон формы авторизации

--- index.php
+++ index.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/CSS/normalize.css">
+    <link rel="stylesheet" type="text/css" href="assets/CSS/style.css">
     <title>Авторизация</title>
 </head>
 <body>
-    
+    <header>
+
+    </header>
+    <main>
+        <div class="container">
+            <h1>Авторизация</h1>
+            <form class="form_authorization" action="#" method="post">
+                <label for="username">Логин:</label>
+                <input type="text" name="username" placeholder="Введите логин:">
+                <label for="password">Пароль:</label>
+                <input type="password" name="password" placeholder="Введите пароль:">
+                <button type="submit">Войти</button>
+            </form>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
