7a194b8ea0387f8c83ac509b57b5b19634d08500
UmkaBear <dfakhiev@yandex.ru>
2024-03-30 17:24:54 +0500

    Изменение формы авторизации

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="assets/CSS/normalize.css">
     <link rel="stylesheet" type="text/css" href="assets/CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
     <title>Авторизация</title>
 </head>
 <body>
@@ -15,11 +18,23 @@
         <div class="container">
             <h1>Авторизация</h1>
             <form class="form_authorization" action="#" method="post">
-                <label for="username">Логин:</label>
-                <input type="text" name="username" placeholder="Введите логин:">
-                <label for="password">Пароль:</label>
-                <input type="password" name="password" placeholder="Введите пароль:">
-                <button type="submit">Войти</button>
+                <div class="login">
+                    <div>
+                        <label for="username">Логин:</label>
+                    </div>
+                    <div>
+                        <input type="text" name="username" placeholder="Введите логин:">
+                    </div>
+                </div>
+                <div class="password">
+                    <label for="password">Пароль:</label>
+                    <input type="password" name="password" placeholder="Введите пароль:">
+                </div>
+                <div class="button_form_authorization">
+                    <button type="submit">Войти</button>
+                    <button type="submit">Регистрация</button>
+                </div>
+                
             </form>
         </div>
     </main>
