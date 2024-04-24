779068cbd772d26ee6422b1701d031767b8db4bf
UmkaBear <dfakhiev@yandex.ru>
2024-04-24 14:10:33 +0500

    Api

--- api/login.php
+++ api/login.php
@@ -1,32 +1,28 @@
 <?php
 
 
-// Заголовки
+
 header("Access-Control-Allow-Origin: http://www/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
- 
-// Файлы необходимые для соединения с БД
+
 include_once "Config/Database.php";
 include_once "Objects/User.php";
  
-// Получаем соединение с базой данных
+
 $database = new Database();
 $db = $database->getConnection();
  
-// Создание объекта "User"
+
 $user = new User($db);
- 
-// Получаем данные
+
 $data = json_decode(file_get_contents("php://input"));
- 
-// Устанавливаем значения
+
 $user->userlogin = $data->userlogin;
 $userlogin = $user->userlogin();
- 
-// Подключение файлов JWT
+
 include_once "Config/core.php";
 include_once $_SERVER['DOCUMENT_ROOT']."/vendor/firebase/php-jwt/src/BeforeValidException.php";
 include_once $_SERVER['DOCUMENT_ROOT']."/vendor/firebase/php-jwt/src/ExpiredException.php";
@@ -34,7 +30,7 @@ include_once $_SERVER['DOCUMENT_ROOT']."/vendor/firebase/php-jwt/src/SignatureIn
 include_once $_SERVER['DOCUMENT_ROOT']."/vendor/firebase/php-jwt/src/JWT.php";
 use \Firebase\JWT\JWT;
  
-// Существует ли электронная почта и соответствует ли пароль тому, что находится в базе данных
+
 if ($userlogin && $user->password) {
  
     $token = array(
@@ -49,10 +45,9 @@ if ($userlogin && $user->password) {
        )
     );
  
-    // Код ответа
+
     http_response_code(200);
- 
-    // Создание jwt
+
     $jwt = JWT::encode($token, $key, 'HS256');
     echo json_encode(
         array(
@@ -62,13 +57,12 @@ if ($userlogin && $user->password) {
     );
 }
  
-// Сообщим пользователю, что он не может войти в систему
+
 else {
- 
-  // Код ответа
+
   http_response_code(401);
 
-  // Скажем пользователю что войти не удалось
+
   echo json_encode(array("message" => "Ошибка входа"));
 }
 ?>
--- api/validate_token.php
+++ api/validate_token.php
@@ -1,13 +1,13 @@
 <?php
 
-// Заголовки
+
 header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
-// Требуется для декодирования JWT
+
 include_once "Config/core.php";
 include_once $_SERVER['DOCUMENT_ROOT']."/vendor/firebase/php-jwt/src/BeforeValidException.php";
 include_once $_SERVER['DOCUMENT_ROOT']."/vendor/firebase/php-jwt/src/ExpiredException.php";
@@ -17,38 +17,31 @@ include_once $_SERVER['DOCUMENT_ROOT']."/vendor/firebase/php-jwt/src/Key.php";
 use \Firebase\JWT\JWT;
 use \Firebase\JWT\Key; 
  
-// Получаем значение веб-токена JSON
+
 $data = json_decode(file_get_contents("php://input"));
 
-// Получаем JWT
+
 $jwt = isset($data->jwt) ? $data->jwt : "";
 
-// Если JWT не пуст
+
 if ($jwt) {
  
-    // Если декодирование выполнено успешно, показать данные пользователя
     try {
 
-        // Декодирование jwt
         $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
  
-        // Код ответа
         http_response_code(200);
  
-        // Покажем детали
         echo json_encode(array(
             "message" => "Доступ разрешен",
             "data" => $decoded->data
         ));
     }
  
-    // Если декодирование не удалось, это означает, что JWT является недействительным
     catch (Exception $e) {
     
-        // Код ответа
         http_response_code(401);
     
-        // Сообщим пользователю что ему отказано в доступе и покажем сообщение об ошибке
         echo json_encode(array(
             "message" => "Вам доступ закрыт",
             "error" => $e->getMessage()
@@ -56,12 +49,9 @@ if ($jwt) {
     }
 }
  
-// Покажем сообщение об ошибке, если JWT пуст
 else {
  
-    // Код ответа
     http_response_code(401);
  
-    // Сообщим пользователю что доступ запрещен
     echo json_encode(array("message" => "Доступ запрещён"));
 }
